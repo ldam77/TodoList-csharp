44384051a37d4497a76844611b298a92d2a48c09
Lan Dam <ldam77@yahoo.com>
2018-07-04 15:50:54 -0700

    add list

--- TodoList/Controllers/ItemsController.cs
+++ TodoList/Controllers/ItemsController.cs
@@ -1,22 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
-    public class ItemsController : Controller
+  public class ItemsController : Controller
+  {
+    [HttpGet("/items")]
+    public ActionResult Index()
     {
-
-        [HttpGet("/items")]
-        public ActionResult Index()
-        {
-            Item newItem = new Item(Request.Query["new-item"]);
-            return View(newItem);
-        }
-
-        [HttpGet("/items/new")]
-        public ActionResult CreateForm()
-        {
-            return View();
-        }
+        return View();
     }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+        return View();
+    }
+    [HttpPost("/items")]
+    public ActionResult Create()
+    {
+      Item newItem = new Item (Request.Form["new-item"]);
+      newItem.Save();
+      List<Item> allItems = Item.GetAll();
+      return View("Index", allItems);
+    }
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
+    }
+  }
 }
